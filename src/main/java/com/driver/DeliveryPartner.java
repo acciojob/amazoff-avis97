c53e2d49b963f529ccc47e98c93b05fa2509a216
Avishek <savishek@gmail.com>
2023-02-25 17:14:06 +0530

    amazoff

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -1,6 +1,6 @@
 package com.driver;
 
-public class DeliveryPartner {
+public class DeliveryPartner{
 
     private String id;
     private int numberOfOrders;
@@ -14,11 +14,23 @@ public class DeliveryPartner {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public Integer getNumberOfOrders(){
         return numberOfOrders;
     }
 
     public void setNumberOfOrders(Integer numberOfOrders) {
         this.numberOfOrders = numberOfOrders;
+    }
+
+    @Override
+    public String toString() {
+        return "DeliveryPartner{" +
+                "id='" + id + '\'' +
+                ", numberOfOrders=" + numberOfOrders +
+                '}';
     }
 }
---
+++ src/main/java/com/driver/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.driver;
+
+import java.util.*;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderRepository{
+
+
+}
